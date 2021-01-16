341e7f1d892b06da34723c82c1487ad0f353fc38
Nurcan Ozkan <34343349+nurcanozkann@users.noreply.github.com>
2021-01-16 15:29:49 +0300

    Update SessionManager.cs

--- ObiletcomProject.BusinessLayer/Concrete/SessionManager.cs
+++ ObiletcomProject.BusinessLayer/Concrete/SessionManager.cs
@@ -16,8 +16,8 @@ namespace ObiletcomProject.BusinessLayer.Concrete
                 client.Timeout = -1;
                 var request = new RestRequest(Method.POST);
                 request.AddHeader("Content-Type", "application/json");
-                request.AddHeader("Authorization", "Basic ZEdocGMybHpZV0p5WVc1a2JtVjNZbWx1 ");
-                request.AddParameter("application/json", "{\r\n \"type\":1,\r\n \"connection\":{\r\n \"ip-address\":\"145.214.41.21\",\r\n \"port\":\"5117\"\r\n },\r\n \"browser\":{\r\n \"name\":\"Chrome\",\r\n \"version\":\"47.0.0.12\",\r\n \"equipment-id\":\"distribusion\"\r\n }\r\n}", ParameterType.RequestBody);
+                request.AddHeader("Authorization", "Basic blablabla");
+                request.AddParameter("application/json", "{\r\n \"type\":1,\r\n \"connection\":{\r\n \"ip-address\":\"blablabla\",\r\n \"port\":\"5117\"\r\n },\r\n \"browser\":{\r\n \"name\":\"Chrome\",\r\n \"version\":\"47.0.0.12\",\r\n \"equipment-id\":\"distribusion\"\r\n }\r\n}", ParameterType.RequestBody);
                 IRestResponse response = client.Execute(request);
 
                 SessionInfo sessionInfo = JsonConvert.DeserializeObject<SessionInfo>(response.Content);
